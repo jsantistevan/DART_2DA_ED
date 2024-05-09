a4efa480bc3e0f259e0d737c85ce173bb863ede7
JSantistevan <jsantistevan@fastline.com.ec>
2024-05-08 19:08:56 -0500

    merge

--- 7.functions/ejercicios/1.funciones.dart
+++ 7.functions/ejercicios/1.funciones.dart
@@ -1,20 +1,34 @@
-
 void main() {
-  
-  //llama a la funcion
+  /// Llamar a la función
   saludo();
-  // otro metodo para llamar funciones
+  /// Otro método para llamar funciones
   saludo.call();
 
+  /// final suma = 1+1
+  /// print(suma);
   final resultadoDeLaSuma = suma();
-  print("el resultado es : $resultadoDeLaSuma");
-  print(suma());
+  // print("El resutado es: $resultadoDeLaSuma");
+  print(suma.call());
 }
 
-void saludo(){
-  print("hola otra funcion");
+
+void saludo() {
+  print("Hola otra función");
 }
 
-double suma(){
-  return 20 + 1;
+/// Esta es una documentación 
+/// 
+/// Es mi función de suma 
+/// 
+/// Siempre llamarla así: suma();
+/// 
+double suma() {
+  return 20 + 1.6;
 }
+
+String? dia() {
+  final String? lunes = 'Lunes';
+  return lunes;
+}
+
+
